478821edab9711d7c6d2ce387307f8a6a2a21e87
wloszynskiszymon <ace3szymon@gmail.com>
2025-06-02 16:29:19 +0200

    Pokemon List View tests

---
+++ cypress/e2e/pokemon-list-view.cy.ts
@@ -0,0 +1,30 @@
+describe('Pokedex App', () => {
+  it('should display the title', () => {
+    cy.intercept('GET', '**/cards*', { fixture: 'pokemon-list.json' }).as(
+      'getAllPokemons'
+    );
+    cy.visit('http://localhost:4200/');
+    cy.get('h1').contains('Welcome!');
+  });
+
+  it('should display the list of Pokemons', () => {
+    cy.intercept('GET', '**/cards*', { fixture: 'pokemon-list.json' }).as(
+      'getAllPokemons'
+    );
+    cy.visit('http://localhost:4200/');
+    cy.wait('@getAllPokemons', { timeout: 10000 });
+    cy.get('[data-cy=pokemon-card]')
+      .should('exist')
+      .should('have.length.greaterThan', 0);
+  });
+
+  it('should display no results fallback', () => {
+    cy.intercept('GET', '**/cards*', { fixture: 'pokemon-empty-list.json' }).as(
+      'getEmptyPokemons'
+    );
+    cy.visit('http://localhost:4200/');
+    cy.wait('@getEmptyPokemons', { timeout: 10000 });
+    cy.get('[data-cy=pokemon-card]').should('not.exist');
+    cy.get('[data-cy=pokemon-no-results]').should('exist');
+  });
+});
